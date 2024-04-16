dcb67bdbb08fe0f5c539891917b964cb055189cd
matgus217 <matgus217@gmail.com>
2024-04-16 08:52:12 +0000

    Add function in footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer>
+            <p>Made To Simplify Your Day. By Matilda Djärv.</p>
+        </footer>
+    )
+}
+
+export default Footer
