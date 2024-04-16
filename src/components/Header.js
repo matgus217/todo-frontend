a29b5803c6d6406ef2a0540a059fe14e7a9da681
matgus217 <matgus217@gmail.com>
2024-04-16 08:52:42 +0000

    Add function in header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <h1 className="header-title">Get It Done!</h1>
+            <p className="header-p">Add your day. Get everything done.</p>
+        </header>
+    )
+}
+
+export default Header
